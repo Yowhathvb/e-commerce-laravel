008415854b016c7056a90be131de27772ebe6b09
zhar <bwfzbw@gmail.com>
2025-05-08 11:46:39 +0700

    jars

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Edit Word</title>
+    <title>Edit Word dua</title>
 </head>
 <body>
     <h1>Edit Word Document</h1>
--- process.php
+++ process.php
@@ -70,3 +70,15 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     echo "<br><a href='index.php'>Back</a>";
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
