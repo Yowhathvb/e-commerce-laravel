d62e165f86acf13679d79b1658242d58854aa473
zhar <bwfzbw@gmail.com>
2025-05-08 11:45:48 +0700

    jars

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Edit Word Document</title>
+    <title>Edit Word</title>
 </head>
 <body>
     <h1>Edit Word Document</h1>
